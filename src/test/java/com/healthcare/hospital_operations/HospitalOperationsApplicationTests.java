f3e53f16b236300205aaac6069b3c05333bfed45
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-01-01 10:47:53 +0100

    update

--- src/test/java/com/healthcare/hospital_operations/HospitalOperationsApplicationTests.java
+++ src/test/java/com/healthcare/hospital_operations/HospitalOperationsApplicationTests.java
@@ -3,9 +3,10 @@ package com.healthcare.hospital_operations;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@Profile("test")
+@ActiveProfiles("test")
 class HospitalOperationsApplicationTests {
 
 	@Test
